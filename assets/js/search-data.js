792af721762eb1249dd15f3a5459d835b5f21d82
scanilang <sara.canilang@gmail.com>
2026-05-20 02:26:02 +0000

    Deploying to gh-pages from @ scanilang/scanilang.github.io@fd38ff68f3bcef1b59d296c87dac8470bb170a76 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-",
-          title: "",
+  },{id: "nav-research",
+          title: "Research",
           description: "",
           section: "Navigation",
           handler: () => {
